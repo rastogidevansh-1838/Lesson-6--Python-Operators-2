0671fbbe2236d8887af621b7191db4ee9c130821
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2025-12-14 10:21:02 +0530

    whoooh

---
+++ not_same.py
@@ -0,0 +1,18 @@
+a=34567890
+b=34567890
+c=234678
+print (a !=b)
+print (b !=c)
+d= "your wishhh"
+e="not your wish i will decide"
+if (d !=e):
+ print ("d does not match e") 
+ f=34567890
+ g=1233333333333333333333467
+if (a==1) != (b==5):
+ print("santa claus is here")
+h= int(input("enter a number:"))
+if h%2 !=0:
+ print("not an even number")
+else:
+ print ("yaaay it is an even number")
