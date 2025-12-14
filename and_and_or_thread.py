fff79e01e0c66578a99c74294aefc3bd918c75b5
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2025-12-14 10:06:59 +0530

    first commit

---
+++ and_and_or_thread.py
@@ -0,0 +1,18 @@
+a= 67
+b= 78
+c=0
+if a and b and c:
+ print("This information is True")
+else:
+ print("This information is False")
+d= 783
+e = 282
+f= 0
+if a > 0 or b > 0:
+ print("one number is greater then 0")
+else:
+ print("no numbers are greater then 0")
+if b > 0 or c > 0:
+ print("one number is greater then 0")
+else:
+ print("NO number is greater then 0")
