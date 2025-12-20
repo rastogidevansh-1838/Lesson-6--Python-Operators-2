84872872f7b7245071a0b46cbce61213492045b8
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2025-12-20 08:51:59 +0530

    alphabetic formulaaaassss code

---
+++ alphabet.py
@@ -0,0 +1,5 @@
+Alphabet = input("Please Enter Your Own Character : ")
+if((Alphabet >= 'a' and Alphabet <= 'z') or (Alphabet >= 'A' and Alphabet <= 'Z')):
+    print("The Given Character " "is an Alphabet")
+else:
+    print("The Given Character " "is Not an Alphabet")
